f0181fb62e60196e10fa01a2678ef5efe6206fe7
YanisYanisYanisYanisYanis <47368427+YanisYanisYanisYanisYanis@users.noreply.github.com>
2019-02-07 23:14:47 +0100

    Added a help command and bot join/leave events
    I added a help command that gets DMed to the user who did the command
    I also added an event that sends a DM to a server owner when the bot is added to their server, as well as when it's removed from it
    
    I tried fixing an issue where the bot would crash if we DMed it the status command, it work mostly, but gets triggered by the help command so I need to find a way to fix that
    
    I also still need a way to shorten both commands
    
    Remember that I have absolutely no knowledge of Javascript and that this bot is the product of hours of making research through the crappy discord.js documentation, lots of failed tries for each command, and mostly the result of me dedicating a lot of time for a useless challenge.
    Any reviews, tips or critics are appreciated

--- index.js
+++ index.js
@@ -1,35 +1,122 @@
-const Discord = require('discord.js');
+ // Loads everything necessary for the bot to work properly
+ const Discord = require('discord.js');
  const client = new Discord.Client();
- const prefix = "??";
+ const config = require("./config.json");
 
+
+
+// Logs the bot's tag and the number of server he's on
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
+ console.log(`Logged in as ${client.user.tag} on ${client.guilds.size} servers!`);
  });
-// Set the bot's "Playing: " status (must be in an event!)
-client.on("ready", () => {
-    client.user.setActivity("pewdiepie", { type: "WATCHING"})
+// Make the bot says in it's status how many server's it's on
+client.on(`ready`, () => {
+    client.user.setActivity(`${client.guilds.size} servers!`, {type: "WATCHING"})
+})
+
+// Logs when the bot joins a server and sends a message to the server owner
+client.on(`guildCreate`, guild => {
+    console.log(`I just got added to "${guild.name}" ${guild.id}! Owner: ${guild.owner.user.tag}/${guild.owner.id}`)
+    guild.owner.send(`Hey! Thank you for adding me to your server!
+For now I'm still being developped, so I don't have a lot of commands available, but I will be able to do more things in the future :D
+If you'd like a list of all the commands available, you just need to send me **??help** !
+Any questions ? Send a DM to my creator <@284699100945842176> !`)
+});
+
+// Logs when the bot leaves a server and sends a message to the server owner
+client.on(`guildDelete`, guild => {
+  guild.owner.send(`Hey, I noticed that you removed me from your server. If you didn't find me useful, that's okay!
+But if it's because you encountered a problem, then you can send a DM to my creator <@284699100945842176>
+He will probably know how to resolve the issue you had.`)
+    console.log(`I just got removed from "${guild.name}" ${guild.id}`)
 })
 
 
-client.on("message", (message) => {
 
-  // Exit and stop if the prefix's not there
-  if (!message.content.startsWith(prefix)) return;
 
-  // Detects "status" and grabs the role from the sender
-  if (message.content.startsWith(prefix + "status")) {
-  	 let myRole = message.guild.roles.get("541685253006295055");
 
-  // If the sender doesn't have the role
-  if (!message.member.roles.has("541685253006295055")) {
-  	  message.channel.send("You do not have the right role!")
-  	  console.log(`${message.member.user.tag} has issued ??status and failed!`)};
 
-  // If the sender has the role
-  if (message.member.roles.has("541685253006295055")) {
-     message.channel.send("```Working.```")
-     console.log(`${message.member.user.tag} has issued ??status and succeded!`)};
-  }
+
+
+// Status command (pretty useless, but it's working, mostly), might need to shorten it
+client.on(`message`, (message) => {
+
+    // 1/2 Tells the user he needs to use the command on a server in order for it to work
+    if (message.channel.type === `dm`)
+    message.author.send(`You can only perform this command on a server!`);
+
+    // 2/2 Exits and stops if the command was sent in DM (Those 2 get triggered by the other commands, I still need to fix that)
+    if (message.channel.type === `dm`) return;
+
+    // Exits and stops if the message's author is a bot
+    if(message.author.bot) return;
+
+    // Exits and stops if the prefix's not there
+    if (!message.content.startsWith(config.prefix)) return;
+
+    // Exits and stops if this command isn't used (to avoid the bot confusing another command with this one)
+    if (!message.content.startsWith(config.prefix + `status`)) return;
+
+    // Detects "status" and grabs the role from the sender
+    let myRole = null;
+    if (message.content.startsWith(config.prefix + `status`)) {
+        message.guild.roles.get(config.botaccess);
+        myRole = message.member.roles.has(config.botaccess)
+    }
+       
+    // If the sender doesn't have the role
+    if (!myRole) {
+        message.channel.send(`You do not have the right role!`);
+        console.log(`${message.member.user.tag}/${message.author} has issued ??status and failed!`)
+    }
+    else 
+    {
+        // If the sender has the role
+        message.channel.send(`Working.`);
+        console.log(`${message.member.user.tag}/${message.author} has issued ??status and succeeded!`);
+    }        
 });
 
-client.login('your precious token right here');
+
+
+
+
+
+
+
+// Help command
+// There might be a way to shorten this bit, I'll get back to it later
+client.on(`message`, (message) => {
+
+    // Exits and stops if the prefix's not there
+    if (!message.content.startsWith(config.prefix)) return;
+
+    // Exits and stops if the message's author is a bot
+    if(message.author.bot) return;
+
+    // Exits and stops if this command isn't used (to avoid the bot confusing another command with this one)
+    if (!message.content.startsWith(config.prefix + `help`)) return;
+
+    // Detects "help"
+    if (message.content.startsWith(config.prefix + `help`))
+
+    // If the command was sent in a server
+    if (message.channel.type === `text`) {
+      message.channel.send(`Check your DMs!`)
+      message.author.send(`Here is a list of all the commands available!
+**??help** : displays all the available commands for the bots
+**??status** : displays the status of the bot! (it's useless but I did so I'm proud :D)`)
+    }
+    else
+    {
+      // If the command was sent in DM to the bot
+      message.author.send(`Here is a list of all the commands available!
+**??help** : displays all the available commands for the bots
+**??status** : displays the status of the bot! (it's useless but I did so I'm proud :D)`)
+    }
+});
+
+
+
+// The bot's token (here, in an external config file)
+client.login(config.token);
