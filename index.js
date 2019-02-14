33a77638775b5cbca46377ee070494be514c227d
yanis <47368427+YanisYanisYanisYanisYanis@users.noreply.github.com>
2019-02-14 02:41:34 +0100

    Added "servers" command
    The biggest change here is the addition of a ??servers command.
    When performed, the bot sends a message (in the channel the command was performed in) which contains a list of every server the bot is on.
    
    I've went through 2 versions of this command, the first one would send each servers in a different message with a delay between them (my bad internet caused the delay to be pretty consequent). So I had to find a way to make it so it's only one message.
    Someone on Discord told me what the method to do it was (without any actual code).
    I started by searching in the discord.js documentation how to add something to an already existing string, found string.concat(), and then spent a lot of time figuring out how it worked.
    Then I had troubles figuring out of variables worked, when I finally understood, I experimented a lot, then re read the advice I was given, and BAM! It then only took me around 15 minutes to come up with this.
    Took me around 5 hours to do
    
    Then replaced all returns to the line with \n.
    And also made it so that the bot's status automatically updates every time it's added or removed from a server.
    
    Next thing I'll do is separate each commands into a separate file.
    My Discord: @Yanis#7227

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ client.on('ready', () => {
  client.user.setActivity(`${client.guilds.size} servers!`, {type: "WATCHING"});
  });
 
-
-
 // Logs when the bot joins a server and sends a message to the server owner
 client.on(`guildCreate`, guild => {
     console.log(`I just got added to "${guild.name}" ${guild.id}! Owner: ${guild.owner.user.tag}/${guild.owner.id}`)
@@ -20,6 +18,7 @@ client.on(`guildCreate`, guild => {
 For now I'm still being developped, so I don't have a lot of commands available, but I will be able to do more things in the future :D
 If you'd like a list of all the commands available, you just need to send me __??help__ !
 Any questions ? Send a DM to my creator <@284699100945842176> !`);
+	client.user.setActivity(`${client.guilds.size} servers!`, {type: "WATCHING"});
 });
 
 // Logs when the bot leaves a server and sends a message to the server owner
@@ -28,9 +27,8 @@ client.on(`guildDelete`, guild => {
     guild.owner.send(`Hey, I noticed that you removed me from your server. If you didn't find me useful, that's okay!
 But if it's because you encountered a problem, then you can send a DM to my creator <@284699100945842176>
 He will probably know how to resolve the issue you had.`);
+    client.user.setActivity(`${client.guilds.size} servers!`, {type: "WATCHING"});
 });
-
-
 
 
 
@@ -44,18 +42,43 @@ client.on(`message`, async (message) => {
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
+
+
+
+    // Lists all the servers the bot is in
+    if (command === `servers`) {
+
+        // Checks if I'm the one performing the command
+        let owner = message.author.id === config.yanis
+        if (owner) {
+
+            // Creates two variables that will  be used in the command
+            var guildNames;
+            var server = `Here are all the servers I'm in!\n`
+            // For every guild the bot's on, adds the guild's name to the server variable
+            client.guilds.forEach((guild) => {
+                guildNames = (' - ' + guild.name + `\n`);
+                server = server.concat(guildNames)
+            })
+            // Sends the final message
+            message.channel.send(server);
+        }
+
+    };
+
+
+
+
     // Help command! :3
     if (command === 'help') {
 
     	if (message.channel.type != `dm`) {
-        await message.channel.send('Check your DMs! :-)');
+        await message.channel.send('Check your DMs! 🙃');
     	}
 
         // Added return to prevent the bot checking for any other command.
-        return message.author.send(`Here is a list of all the commands available!
-__??help__ : displays all the available commands for the bots
-__??status__ : displays the status of the bot! (it's useless but I did so I'm proud :D)`);
-    }
+        return message.author.send(`Here is a list of all the commands available!\n__??help__ : displays all the available commands for the bots\n__??status__ : displays the status of the bot! (it's useless but I did it so I'm proud :D)`);
+    };
 
 
 
@@ -65,7 +88,7 @@ __??status__ : displays the status of the bot! (it's useless but I did so I'm pr
 
     if (command === `status`) {
         // Detects "status" and grabs the role from the sender
-        let isAdmin = message.member.permissions.has(`ADMINISTRATOR`);
+        let isAdmin = message.member.permissions.has(`ADMINISTRATOR`) || message.member.id != config.yanis;
 
         // If the sender doesn't have the role
         if (!isAdmin) {
@@ -77,6 +100,7 @@ __??status__ : displays the status of the bot! (it's useless but I did so I'm pr
         return message.channel.send(`Working.`);
     }
 });
+
 
 
 
